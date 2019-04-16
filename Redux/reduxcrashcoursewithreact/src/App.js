46a803f7f2f1d58a5a03f5e3f0ded2ba120887a3
Qparrot <parrot.parrot@gmail.com>
2019-04-16 16:17:41 +0200

    Add the redux store in App.js

--- Redux/reduxcrashcoursewithreact/src/App.js
+++ Redux/reduxcrashcoursewithreact/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware } from 'redux';
 import logo from './logo.svg';
 import './App.css';
+
 import Posts from './components/Posts';
 import Postform from './components/Postform';
 
+const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
-    return (
+	  return (
+		  <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -28,6 +33,7 @@ class App extends Component {
 	 <Posts />
  </body>
       </div>
+      </Provider>
     );
   }
 }
