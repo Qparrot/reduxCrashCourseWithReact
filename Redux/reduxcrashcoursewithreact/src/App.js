cd41410acb11cd02a80c08f856f84a7f70bdc859
Qparrot <parrot.parrot@gmail.com>
2019-04-22 21:02:47 +0200

    Erase files

--- Redux/reduxcrashcoursewithreact/src/App.js
+++ Redux/reduxcrashcoursewithreact/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import {createStore, applyMiddleware } from 'redux';
 import logo from './logo.svg';
 import './App.css';
-
 import Posts from './components/Posts';
 import Postform from './components/Postform';
 
-const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
-	  return (
-		  <Provider store={store}>
+    return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -33,7 +28,6 @@ class App extends Component {
 	 <Posts />
  </body>
       </div>
-      </Provider>
     );
   }
 }
